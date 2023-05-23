b481d5d6b3702c42bf5765d2e8169c929f790910
hongju-jeong <sub06038@naver.com>
2023-05-23 15:31:52 +0000

    base network

--- data/dataset_jpeg.py
+++ data/dataset_jpeg.py
@@ -1,20 +1,17 @@
 import random
+import numpy as np
+import torch
 import torch.utils.data as data
 import utils.utils_image as util
 import cv2
 
-
 class DatasetJPEG(data.Dataset):
+
     def __init__(self, opt):
         super(DatasetJPEG, self).__init__()
-        print('Dataset: JPEG compression artifact reduction (deblocking) with quality factor. Only dataroot_H is needed.')
         self.opt = opt
         self.n_channels = opt['n_channels'] if opt['n_channels'] else 3
-        self.patch_size = self.opt['H_size'] if opt['H_size'] else 128
-
-        self.quality_factor = opt['quality_factor'] if opt['quality_factor'] else 40
-        self.quality_factor_test = opt['quality_factor_test'] if opt['quality_factor_test'] else 40
-        self.is_color = opt['is_color'] if opt['is_color'] else False
+        self.patch_size = self.opt['H_size'] if opt['H_size'] else 64
 
         # -------------------------------------
         # get the path of H, return None if input is None
@@ -22,59 +19,67 @@ class DatasetJPEG(data.Dataset):
         self.paths_H = util.get_image_paths(opt['dataroot_H'])
 
     def __getitem__(self, index):
+        # -------------------------------------
+        # get H image
+        # -------------------------------------
+        H_path = self.paths_H[index]
+        img_H = util.imread_uint(H_path, self.n_channels)
+
+        L_path = H_path
 
         if self.opt['phase'] == 'train':
-            # -------------------------------------
-            # get H image
-            # -------------------------------------
-            H_path = self.paths_H[index]
-            img_H = util.imread_uint(H_path, 3)
-            L_path = H_path
-
+            """
+            # --------------------------------
+            # get L/H/M patch pairs
+            # --------------------------------
+            """
             H, W = img_H.shape[:2]
-            self.patch_size_plus = self.patch_size + 8
 
             # ---------------------------------
-            # randomly crop a large patch
+            # randomly crop the patch
             # ---------------------------------
-            rnd_h = random.randint(0, max(0, H - self.patch_size_plus))
-            rnd_w = random.randint(0, max(0, W - self.patch_size_plus))
-            patch_H = img_H[rnd_h:rnd_h + self.patch_size_plus, rnd_w:rnd_w + self.patch_size_plus, ...]
+            self.patch_size_plus8 = self.patch_size+8
+            # ---------------------------------
+            # randomly crop the patch
+            # ---------------------------------
+            rnd_h = random.randint(0, max(0, H - self.patch_size_plus8))
+            rnd_w = random.randint(0, max(0, W - self.patch_size_plus8))
+            patch_H = img_H[rnd_h:rnd_h + self.patch_size_plus8, rnd_w:rnd_w + self.patch_size_plus8, :]
 
             # ---------------------------------
             # augmentation - flip, rotate
             # ---------------------------------
             mode = random.randint(0, 7)
             patch_H = util.augment_img(patch_H, mode=mode)
+#            if random.random() > 0.5:
+#                patch_H = self.jitter(util.uint2tensor4(patch_H))
+#                patch_H = util.tensor2uint(patch_H)
 
             # ---------------------------------
             # HWC to CHW, numpy(uint) to tensor
             # ---------------------------------
+#            img_H = util.uint2tensor3(patch_H)
+#            img_L = img_H.clone()
             img_L = patch_H.copy()
+            img_H = patch_H.copy()
 
             # ---------------------------------
-            # set quality factor
+            # get noise level
             # ---------------------------------
-            quality_factor = self.quality_factor
 
-            if self.is_color:  # color image
-                img_H = img_L.copy()
-                img_L = cv2.cvtColor(img_L, cv2.COLOR_RGB2BGR)
-                result, encimg = cv2.imencode('.jpg', img_L, [int(cv2.IMWRITE_JPEG_QUALITY), quality_factor])
-                img_L = cv2.imdecode(encimg, 1)
-                img_L = cv2.cvtColor(img_L, cv2.COLOR_BGR2RGB)
+            if random.random() > 0.75:
+                quality_factor = random.randint(8, 96)
             else:
-                if random.random() > 0.5:
-                    img_L = util.rgb2ycbcr(img_L)
-                else:
-                    img_L = cv2.cvtColor(img_L, cv2.COLOR_RGB2GRAY)
-                img_H = img_L.copy()
-                result, encimg = cv2.imencode('.jpg', img_L, [int(cv2.IMWRITE_JPEG_QUALITY), quality_factor])
-                img_L = cv2.imdecode(encimg, 0)
+                quality_factor = random.choice([10,20,30,40,50,60])
 
-            # ---------------------------------
-            # randomly crop a patch
-            # ---------------------------------
+            noise_level = (100-quality_factor)/100.0
+            img_L = cv2.cvtColor(img_L, cv2.COLOR_RGB2BGR)
+            result, encimg = cv2.imencode('.jpg', img_L, [int(cv2.IMWRITE_JPEG_QUALITY), quality_factor])
+            img_L = cv2.imdecode(encimg, 1)
+            img_L = cv2.cvtColor(img_L, cv2.COLOR_BGR2RGB)
+
+            noise_level = torch.FloatTensor([noise_level])
+
             H, W = img_H.shape[:2]
             if random.random() > 0.5:
                 rnd_h = random.randint(0, max(0, H - self.patch_size))
@@ -84,35 +89,39 @@ class DatasetJPEG(data.Dataset):
                 rnd_w = 0
             img_H = img_H[rnd_h:rnd_h + self.patch_size, rnd_w:rnd_w + self.patch_size]
             img_L = img_L[rnd_h:rnd_h + self.patch_size, rnd_w:rnd_w + self.patch_size]
+
+
+            img_L, img_H = util.uint2tensor3(img_L), util.uint2tensor3(img_H)
+
+            # ---------------------------------
+            # add noise
+            # ---------------------------------
+#            noise = torch.randn(img_L.size()).mul_(noise_level).float()
+#            img_L.add_(noise)
+
         else:
+            """
+            # --------------------------------
+            # get L/H/M image pairs
+            # --------------------------------
+            """
+            img_L = img_H.copy()
+            
+            
+            quality_factor = 10
+            noise_level = (100-quality_factor)/100.0
+            img_L = cv2.cvtColor(img_L, cv2.COLOR_RGB2BGR)
+            result, encimg = cv2.imencode('.jpg', img_L, [int(cv2.IMWRITE_JPEG_QUALITY), quality_factor])
+            img_L = cv2.imdecode(encimg, 1)
+            img_L = cv2.cvtColor(img_L, cv2.COLOR_BGR2RGB)
 
-            H_path = self.paths_H[index]
-            L_path = H_path
-            # ---------------------------------
-            # set quality factor
-            # ---------------------------------
-            quality_factor = self.quality_factor_test
+            noise_level = torch.FloatTensor([noise_level])
 
-            if self.is_color:  # color JPEG image deblocking
-                img_H = util.imread_uint(H_path, 3)
-                img_L = img_H.copy()
-                img_L = cv2.cvtColor(img_L, cv2.COLOR_RGB2BGR)
-                result, encimg = cv2.imencode('.jpg', img_L, [int(cv2.IMWRITE_JPEG_QUALITY), quality_factor])
-                img_L = cv2.imdecode(encimg, 1)
-                img_L = cv2.cvtColor(img_L, cv2.COLOR_BGR2RGB)
-            else:
-                img_H = cv2.imread(H_path, cv2.IMREAD_UNCHANGED)
-                is_to_ycbcr = True if img_L.ndim == 3 else False
-                if is_to_ycbcr:
-                    img_H = cv2.cvtColor(img_H, cv2.COLOR_BGR2RGB)
-                    img_H = util.rgb2ycbcr(img_H)
+            noise_level_map = torch.ones((1, img_L.shape[0], img_L.shape[0])).mul_(noise_level).float()
+            img_L, img_H = util.uint2tensor3(img_L), util.uint2tensor3(img_H)
 
-                result, encimg = cv2.imencode('.jpg', img_H, [int(cv2.IMWRITE_JPEG_QUALITY), quality_factor])
-                img_L = cv2.imdecode(encimg, 0)
 
-        img_L, img_H = util.uint2tensor3(img_L), util.uint2tensor3(img_H)
-
-        return {'L': img_L, 'H': img_H, 'L_path': L_path, 'H_path': H_path}
+        return {'L': img_L, 'H': img_H, 'qf': noise_level, 'L_path': L_path, 'H_path': H_path}
 
     def __len__(self):
         return len(self.paths_H)
---
+++ main_train_fbswinir.py
@@ -0,0 +1,105 @@
+import os.path
+import math
+import argparse
+import time
+import random
+import numpy as np
+from collections import OrderedDict
+import logging
+import torch
+from torch.utils.data import DataLoader
+
+from utils import utils_logger
+from utils import utils_image as util
+from utils import utils_option as option
+
+from data.select_dataset import define_Dataset
+from models.select_model import define_Model
+
+def main(json_path='options/swinir/train_fbswinir_color.json'):
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-opt', type=str, default=json_path, help='Path to option JSON file.')
+    
+    opt = option.parse(parser.parse_args().opt, is_train=True)
+    util.mkdirs((path for key, path in opt['path'].items() if 'pretrained' not in key))
+    
+    # ----------------------------------------
+    # update opt
+    # ----------------------------------------
+    # -->-->-->-->-->-->-->-->-->-->-->-->-->-
+    init_iter_G, init_path_G = option.find_last_checkpoint(opt['path']['models'], net_type='G')
+    init_iter_E, init_path_E = option.find_last_checkpoint(opt['path']['models'], net_type='E')
+    opt['path']['pretrained_netG'] = init_path_G
+    opt['path']['pretrained_netE'] = init_path_E
+    init_iter_optimizerG, init_path_optimizerG = option.find_last_checkpoint(opt['path']['models'], net_type='optimizerG')
+    opt['path']['pretrained_optimizerG'] = init_path_optimizerG
+    current_step = max(init_iter_G, init_iter_E, init_iter_optimizerG)
+    
+    border = opt['scale']
+    # --<--<--<--<--<--<--<--<--<--<--<--<--<-
+    
+    # ----------------------------------------
+    # save opt to  a '../option.json' file
+    # ----------------------------------------
+    option.save(opt)
+    
+    # ----------------------------------------
+    # return None for missing key
+    # ----------------------------------------
+    opt = option.dict_to_nonedict(opt)
+    
+    # ----------------------------------------
+    # configure logger
+    # ----------------------------------------
+    logger_name = 'train'
+    utils_logger.logger_info(logger_name, os.path.join(opt['path']['log'], logger_name+'.log'))
+    logger = logging.getLogger(logger_name)
+    logger.info(option.dict2str(opt))
+    
+    # ----------------------------------------
+    # seed
+    # ----------------------------------------
+    seed = opt['train']['manual_seed']
+    if seed is None:
+        seed = random.randint(1, 10000)
+    logger.info('Random seed: {}'.format(seed))
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    
+    
+    # ----------------------------------------
+    # 1) create_dataset
+    # 2) creat_dataloader for train and test
+    # ----------------------------------------
+    for phase, dataset_opt in opt['datasets'].items():
+        if phase == 'train':
+            train_set = define_Dataset(dataset_opt)
+            train_size = int(math.ceil(len(train_set) / dataset_opt['dataloader_batch_size']))
+            logger.info('Number of train images: {:,d}, iters: {:,d}'.format(len(train_set), train_size))
+            
+            train_loader = DataLoader(train_set,
+                                          batch_size=dataset_opt['dataloader_batch_size'],
+                                          shuffle=dataset_opt['dataloader_shuffle'],
+                                          num_workers=dataset_opt['dataloader_num_workers'],
+                                          drop_last=True,
+                                          pin_memory=True)
+        
+        elif phase == 'test':
+            test_set = define_Dataset(dataset_opt)
+            test_loader = DataLoader(test_set, batch_size=1,
+                                     shuffle=False, num_workers=1,
+                                     drop_last=False, pin_memory=True)
+            
+        else:
+            raise NotImplementedError("Phase [%s] is not recognized." % phase)
+            
+            
+            
+            
+            
+            
+    
+    
